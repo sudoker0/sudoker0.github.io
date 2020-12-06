55b7b489c1359a5506cd1af5d97e5b00c72eac32
QuanMCPC <60545346+QuanMCPC@users.noreply.github.com>
2020-12-06 15:41:13 +0700

    .

---
+++ website/script/dist/index.dev.js
@@ -0,0 +1,13 @@
+"use strict";
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('https://quanmcpc.github.io/website/script/serviceworker.js?ver=1.1').then(function (registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    })["catch"](function (err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
