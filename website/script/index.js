8e8d9531730d0f56e578b477bc8904abae1479af
QuanMCPC <60545346+QuanMCPC@users.noreply.github.com>
2020-12-06 14:59:36 +0700

    .

--- website/script/index.js
+++ website/script/index.js
@@ -1,6 +1,6 @@
 if('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('serviceworker.js')
+        navigator.serviceWorker.register('https://quanmcpc.github.io/website/script/serviceworker.js')
             .then((registration) => {
                 // Registration was successful
                 console.log('ServiceWorker registration successful with scope: ', registration.scope);
