194bc76cbaf474995c1510070ebac1cf5e991733
QuanMCPC <60545346+QuanMCPC@users.noreply.github.com>
2020-12-20 11:41:12 +0700

    .

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -2,7 +2,7 @@
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
-    navigator.serviceWorker.register('serviceworker.js?ver=20384').then(function (registration) {
+    navigator.serviceWorker.register('/serviceworker.js?ver=20384').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     })["catch"](function (err) {
