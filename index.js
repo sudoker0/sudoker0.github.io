98a5c6844d1deba4e944b295f4d026c1692ff14e
QuanMCPC <60545346+QuanMCPC@users.noreply.github.com>
2021-03-21 13:56:03 +0700

    Change

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 if('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.getRegistrations().then(r => {
+            for (let registration of r) {
+                registration.unregister()
+            }
+            navigator.serviceWorker.register('/sw.js')
             .then((registration) => { console.log('ServiceWorker registration successful with scope: ', registration.scope) })
             .catch(err => { console.log('ServiceWorker registration failed: ', err) });
+        }).catch(e => {
+            console.log('Service Worker unregistration failed: ', e)
+        });
     });
 }
