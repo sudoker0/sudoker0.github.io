263d97fe1bb0fca1e278d229a72d5c7273800896
QuanMCPC <60545346+QuanMCPC@users.noreply.github.com>
2021-04-02 20:34:59 +0700

    Move the same script and style to seperate file to save space

---
+++ website/script/lite.js
@@ -0,0 +1,51 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'), results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+var name_ = [
+    {
+        "name": "Home",
+        "title": "Home - The main page of the website",
+        "address": "home.html"
+    },
+    {
+        "name": "Credit",
+        "title": "Credit - All of the stuff and people that created this website will be listed here",
+        "address": "credit.html"
+    },
+    {
+        "name": "About Website",
+        "title": "About Website - Information about my website",
+        "address": "about.html"
+    },
+    {
+        "name": "Gallery",
+        "title": "Gallery - Where I store picture of project and stuff",
+        "address": "gallery.html"
+    }
+];
+//Syntax: Name / Description / Link (Space is important)
+name_.forEach(function (list, index) {
+    var dateTime = Math.floor(Math.random() * 1000) + '-' + Math.floor(Math.random() * 1000) + '-' + Math.floor(Math.random() * 1000) + '-' + Math.floor(Math.random() * 1000);
+    var anchor = document.createElement("a"), anchor_text = document.createTextNode(list.name), anchor2 = document.createElement("a"), anchor2_text = document.createTextNode(list.name), anchor3 = document.createElement("br");
+    anchor.appendChild(anchor_text);
+    anchor.title = list.title;
+    anchor.href = list.address + "?id=" + dateTime;
+    anchor2.appendChild(anchor2_text);
+    anchor2.title = list.title;
+    anchor2.href = list.address + "?id=" + dateTime;
+    document.getElementById("accbar").appendChild(anchor);
+});
+window.onload = window.onresize = e => {
+    if (getParameterByName("no_screen_size_check") == "true") {} else {
+        if (window.innerWidth < 320 || window.outerWidth < 320) {
+            document.getElementById("screentoosmall").style.display = "block";
+        } else {
+            document.getElementById("screentoosmall").style.display = "none";
+        }
+    }
+}
