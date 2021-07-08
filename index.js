43626b9d57cb5ada7c5a44334f2e7a6eeb17ce41
QuanMCPC <60545346+QuanMCPC@users.noreply.github.com>
2021-07-08 16:16:58 +0700

    I don't know how to describe this change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 if('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/sw.js?version=5')
+        navigator.serviceWorker.register('/sw.js?version=6')
             .then((registration) => { console.log('ServiceWorker registration successful with scope: ', registration.scope) })
             .catch(err => { console.log('ServiceWorker registration failed: ', err) });
     });
