f4886938caf325ab21250051ab9db26bb7e0c8eb
QuanMCPC <60545346+QuanMCPC@users.noreply.github.com>
2021-07-08 16:05:49 +0700

    Add some comment and remove the "IsDirectory" in the listing

--- get_file_info_for_archive.py
+++ get_file_info_for_archive.py
@@ -1,6 +1,6 @@
-import os, json, time
+import os, json, time # Import important stuff
 from datetime import date
-start_path = "./archive"
+start_path = "./archive" # The path to get metadata
 total_size = 0
 listing = []
 for path, dirs, files in os.walk(start_path):
@@ -9,8 +9,9 @@ for path, dirs, files in os.walk(start_path):
         f = os.path.join(path, f1)
         fs = os.path.getsize(f) #file size
         mtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(os.path.getmtime(f))) + " (GMT +7)" #Last modification time
-        isdir_ = os.path.isdir(f) #Is it a directory?
-        listing.append({"filename": f1, "filesize": fs, "mod_time": mtime, "isdir": isdir_})
+        #isdir_ = os.path.isdir(f) #Is it a directory?
+        #listing.append({"filename": f1, "filesize": fs, "mod_time": mtime, "isdir": isdir_})
+        listing.append({"filename": f1, "filesize": fs, "mod_time": mtime})
 
 data = {
     "type": "dir_listing",
@@ -18,5 +19,5 @@ data = {
     "list": listing
 }
 print(data)
-with open("./archive/file_listing.json", "w") as outfile:
+with open(start_path + "/file_listing.json", "w") as outfile: #Dump metadata listing to JSON file
     json.dump(data, outfile, indent=4)
