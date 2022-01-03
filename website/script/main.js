9e742104749ac7a313cf63255cf2747601a7d57e
QuanMCPC <60545346+QuanMCPC@users.noreply.github.com>
2022-01-03 11:14:42 +0700

    Some change to the website

--- website/script/main.js
+++ website/script/main.js
@@ -4,13 +4,12 @@ function ls_gt(i) { return localStorage.getItem(i); }
 function ls_st(i, v) { localStorage.setItem(i, v); }
 function setCSSVar(p, v) { document.documentElement.style.setProperty(p, v); }
 var pathname = window.location.pathname, accb_small_isOn = false, nBP = { "close": "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)", "open": "polygon(0 0, 100% 0, 100% 100%, 0% 100%)" };
-document.querySelectorAll("h3#accbar a").forEach(function (v) { document.querySelector("h3#accbar_small").append(v.cloneNode(true)); });
+document.querySelectorAll("h3#accbar a").forEach((v) => { document.querySelector("h3#accbar_small").append(v.cloneNode(true)); });
 function sanitizeString(str) {
     str = str.replace(/[^a-z0-9áéíóúñü \.,_\-\/]/gim, "");
     return str.trim();
 }
-function gPBName(name, url) {
-    if (url === void 0) { url = window.location.href; }
+function gPBName(name, url = window.location.href) {
     if (!url)
         url = window.location.href;
     name = name.replace(/[\[\]\\]/g, '\\$&');
@@ -22,7 +21,7 @@ function gPBName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 function mobileCheck() {
-    var check = false;
+    let check = false;
     (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)))
         check = true; })(navigator.userAgent || navigator.vendor || window["opera"]);
     return check;
@@ -54,7 +53,7 @@ function check() {
     }
     if (gPBName("id") == "entropy") {
         var charStart = 1;
-        setInterval(function () {
+        setInterval(() => {
             var lChar = Math.round(Math.random() * document.body.innerHTML.length), char = rString(Math.floor(charStart));
             document.body.innerHTML = document.body.innerHTML.substring(0, lChar) + char + document.body.innerHTML.substring(lChar + Math.floor(charStart));
             charStart = charStart <= 32 ? charStart + 0.1 : charStart;
@@ -80,15 +79,15 @@ document.addEventListener("click", function (evt) {
     accb_small_isOn = false;
 });
 window.onresize = smtg;
-window.onload = function () {
+window.onload = () => {
     smtg();
-    setTimeout(function () { selAll("nav.navbar").forEach(function (e) { e.classList.add("logo_rotate"); }); }, Math.random() * (4.5e+6 - 3.6e+6) + 3.6e+6);
-    setTimeout(function () { sel("#loadingIndicator").classList.add("lIClosed"); }, 500);
-    document.querySelectorAll("h3#accbar a, h3#accbar_small a").forEach(function (v) {
-        v.addEventListener("click", function (e) {
+    setTimeout(() => { selAll("nav.navbar").forEach(e => { e.classList.add("logo_rotate"); }); }, Math.random() * (4.5e+6 - 3.6e+6) + 3.6e+6);
+    setTimeout(() => { sel("#loadingIndicator").classList.add("lIClosed"); }, 500);
+    document.querySelectorAll("h3#accbar a, h3#accbar_small a").forEach((v) => {
+        v.addEventListener("click", (e) => {
             e.preventDefault();
             sel("#loadingIndicator").classList.remove("lIClosed");
-            setTimeout(function () {
+            setTimeout(() => {
                 window.location.href = e.target["href"];
             }, 500);
         });
@@ -116,8 +115,8 @@ function accb_small() {
 }
 var prevScrollpos = window.pageYOffset;
 var constant = 0;
-function setAttrClass(clas, name, attr) { document.querySelectorAll(".".concat(clas)).forEach(function (elem) { elem.setAttribute(name, attr); }); }
-document.addEventListener("DOMContentLoaded", function () {
+function setAttrClass(clas, name, attr) { document.querySelectorAll(`.${clas}`).forEach((elem) => { elem.setAttribute(name, attr); }); }
+document.addEventListener("DOMContentLoaded", () => {
     if (typeof SVGAnimateElement !== 'undefined') { }
     else {
         sel("#loading_redstone_dust").setAttribute('src', '/website/image/logo/loading.gif');
@@ -126,24 +125,24 @@ document.addEventListener("DOMContentLoaded", function () {
     else {
         setAttrClass("website_logo", "src", "/website/image/logo/logo.png");
     }
-    document.querySelectorAll('.nt').forEach(function (v) {
+    document.querySelectorAll('.nt').forEach((v) => {
         v.classList.remove("nt");
     });
 });
 function dropShadow() {
     sel("#accb").style.boxShadow = scrollY > 5 ? "0px 7px 8px rgb(15 15 15)" : "none";
 }
-document.onscroll = function (_) {
+document.onscroll = (_) => {
     dropShadow();
     sel("#accb_small").style["webkitClipPath"] = nBP.close;
     sel("#accb_small").style.clipPath = nBP.close;
     accb_small_isOn = false;
 };
-sel("#list_dir").onclick = function () { window.location.href = "/dir_listing.html"; };
+sel("#list_dir").onclick = () => { window.location.href = "/dir_listing.html"; };
 var logo_a = selAll("nav#accb > a");
-logo_a.forEach(function (e) {
-    e.onmousemove = function (ev) {
-        selAll("nav.navbar").forEach(function (e) {
+logo_a.forEach(e => {
+    e.onmousemove = (ev) => {
+        selAll("nav.navbar").forEach(e => {
             if (ev.ctrlKey && ev.shiftKey) {
                 e.classList.add("logo_rotate");
             }
@@ -154,16 +153,16 @@ logo_a.forEach(function (e) {
     };
 });
 function oOPIIF(url) {
-    sel("#iframe__").setAttribute("src", sanitizeString(url) + "?nocache=".concat(Math.round(Math.random() * 1000000)));
+    sel("#iframe__").setAttribute("src", sanitizeString(url) + `?nocache=${Math.round(Math.random() * 1000000)}`);
     document.getElementsByTagName("body")[0].style.overflow = "hidden";
     sel("#iframe_").style.display = "block";
-    setTimeout(function () {
+    setTimeout(() => {
         sel("#iframe_").style.transform = "scale(1)";
         sel("#iframe_").style.opacity = "1";
     }, 100);
 }
-selAll("a.openInProjectViewer").forEach(function (v) {
-    v.onclick = function (ev) {
+selAll("a.openInProjectViewer").forEach((v) => {
+    v.onclick = (ev) => {
         ev.preventDefault();
         oOPIIF(v.getAttribute("href"));
     };
@@ -172,7 +171,7 @@ function closeIframe() {
     sel('#iframe__').setAttribute('src', 'about:blank');
     sel('#iframe_').style.transform = 'scale(1.5)';
     sel('#iframe_').style.opacity = '0';
-    setTimeout(function () {
+    setTimeout(() => {
         sel('#iframe_').style.display = 'none';
         sel('body').style.overflow = 'initial';
     }, 400);
