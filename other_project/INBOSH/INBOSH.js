278604e4f0c7ad53f5d3387ae6f0a65caaf019b0
QuanMCPC <60545346+QuanMCPC@users.noreply.github.com>
2022-04-03 16:39:00 +0700

    Some chnage

--- other_project/INBOSH/INBOSH.js
+++ other_project/INBOSH/INBOSH.js
@@ -553,7 +553,6 @@ const commandList = [
         ],
         description: "Calculate using the provided expression and store the result in the provided variable",
         func: (args) => __awaiter(this, void 0, void 0, function* () {
-            var _b;
             if (args["expression"] == undefined) {
                 yield insertString([`[ERROR]: Value not specified.`], true);
             }
@@ -578,7 +577,7 @@ const commandList = [
                     var calc = new Calculation();
                     var result = calc.calculate(i);
                     if (result == undefined || typeof result == "string") {
-                        var error = (_b = result) === null || _b === void 0 ? void 0 : _b.split("\\n");
+                        var error = result === null || result === void 0 ? void 0 : result.split("\\n");
                         yield insertString([`[ERROR]: ${error[0]}`].concat(error.slice(-(error.length - 1))), true);
                     }
                     else {
