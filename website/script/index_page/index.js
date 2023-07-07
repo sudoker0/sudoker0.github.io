44beb1c5bb72733b05213f6badcde2e2928af3e7
QuanMCPC <60545346+sudoker0@users.noreply.github.com>
2023-07-07 15:04:44 +0700

    fix issue with font and some quality of life improvement

--- website/script/index_page/index.js
+++ website/script/index_page/index.js
@@ -8,19 +8,24 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 const content = `// very cool intro:)
+^003$
 import { buttonStyle } from "./style.js"
 
+^003$
 const website = new sudoker0.website()
 const home_page = website.home
 
+^003$
 const a_tag = document.createElement("a")
 a_tag.innerText = "Home"
 a_tag.href = home_page.url
 a_tag.style = buttonStyle
 
+^003$
 this.append(a_tag)
 
-//#run_console`;
+^0001$
+//#console run`;
 const typingDelay = 15;
 function delay(ms) {
     return new Promise(resolve => setTimeout(_ => resolve(), ms));
@@ -29,23 +34,42 @@ function postTyping() {
     return __awaiter(this, void 0, void 0, function* () {
         yield delay(1000);
         document.querySelector("p#running").classList.remove("hidden");
-        yield delay(1000);
+        yield delay(750);
         document.querySelector("a#redirect").classList.remove("hidden");
     });
 }
+function typeWriter(text) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const elm = document.querySelector("pre#codeblock code");
+        for (var i = 0; i < text.length; i++) {
+            var char = text.charAt(i);
+            var nextChar = i < text.length - 1 ? text.charAt(i + 1) : "";
+            var extraDelay = 0;
+            if (char == "\n" && nextChar == "^") {
+                var posNum = 0;
+                i++;
+                while (true) {
+                    i++;
+                    const num = Number(text.charAt(i));
+                    if (isNaN(num)) {
+                        i++;
+                        break;
+                    }
+                    extraDelay += Math.pow(10, posNum) * num;
+                    posNum++;
+                }
+                char = text.charAt(i);
+                console.log(extraDelay);
+            }
+            elm.textContent += char;
+            yield delay(typingDelay + extraDelay);
+            extraDelay = 0;
+        }
+    });
+}
 (() => __awaiter(this, void 0, void 0, function* () {
-    let indexOfString = 0;
-    yield delay(500);
-    const typingInterval = setInterval(() => __awaiter(this, void 0, void 0, function* () {
-        if (indexOfString < content.length) {
-            const char = content.charAt(indexOfString);
-            document.querySelector("pre#codeblock code").textContent += char;
-            indexOfString++;
-        }
-        else {
-            clearInterval(typingInterval);
-            yield postTyping();
-        }
-    }), typingDelay);
+    yield delay(1000);
+    yield typeWriter(content);
+    yield postTyping();
 }))();
 //# sourceMappingURL=index.js.map
--- website/script/index_page/index.ts
+++ website/script/index_page/index.ts
@@ -1,17 +1,22 @@
 const content = `// very cool intro:)
+^003$
 import { buttonStyle } from "./style.js"
 
+^003$
 const website = new sudoker0.website()
 const home_page = website.home
 
+^003$
 const a_tag = document.createElement("a")
 a_tag.innerText = "Home"
 a_tag.href = home_page.url
 a_tag.style = buttonStyle
 
+^003$
 this.append(a_tag)
 
-//#run_console`
+^0001$
+//#console run`
 const typingDelay = 15
 
 function delay(ms: number): Promise<void> {
@@ -21,21 +26,51 @@ function delay(ms: number): Promise<void> {
 async function postTyping() {
     await delay(1000)
     document.querySelector("p#running").classList.remove("hidden")
-    await delay(1000)
+    await delay(750)
     document.querySelector("a#redirect").classList.remove("hidden")
 }
 
-(async () => {
-    let indexOfString = 0
-    await delay(500)
-    const typingInterval = setInterval(async () => {
-        if (indexOfString < content.length) {
-            const char = content.charAt(indexOfString)
-            document.querySelector("pre#codeblock code").textContent += char
-            indexOfString++
-        } else {
-            clearInterval(typingInterval)
-            await postTyping()
+async function typeWriter(text: string) {
+    const elm = document.querySelector("pre#codeblock code")
+    for (var i = 0; i < text.length; i++) {
+        var char = text.charAt(i)
+        var nextChar = i < text.length - 1 ? text.charAt(i + 1) : ""
+        var extraDelay = 0
+
+        if (char == "\n" && nextChar == "^") {
+            var posNum = 0
+            i++
+            while (true) {
+                i++
+                const num = Number(text.charAt(i))
+                if (isNaN(num)) {
+                    i++
+                    break
+                }
+                extraDelay += 10 ** posNum * num
+                posNum++
+            }
+            char = text.charAt(i)
+            console.log(extraDelay)
         }
-    }, typingDelay)
+
+        elm.textContent += char
+        await delay(typingDelay + extraDelay)
+        extraDelay = 0
+    }
+}
+
+// function typeWriter(text: string, index = 0) {
+//     if (index < text.length) {
+//         const char = content.charAt(index)
+//         document.querySelector("pre#codeblock code").textContent += char
+//         index++
+//         setTimeout(() => typeWriter(text, index), typingDelay)
+//     }
+// }
+
+(async () => {
+    await delay(1000)
+    await typeWriter(content)
+    await postTyping()
 })()
